3efd46657b6c8bd40ab22ce3c5f39c45471a9de9
tianshicong <tianshicong@zbintel.net>
2024-08-21 18:39:06 +0800

    api修复

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  baseUrl: process.env.NODE_ENV === 'development' ? '/api/' : '/api/', // 后端接口地址
-  cssUrl: process.env.NODE_ENV === 'development' ? '/api/' : '/api/' // 生成的css文件地址
+  baseUrl: process.env.NODE_ENV === 'development' ? '/api/' : 'https://api.tiansc.top/api/icon/', // 后端接口地址
+  cssUrl: process.env.NODE_ENV === 'development' ? '/api/' : 'https://api.tiansc.top/api/icon/' // 生成的css文件地址
 }
