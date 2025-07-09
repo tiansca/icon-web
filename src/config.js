f4c8f28071ce065de37e2692bf27dc46db8c518c
tianshicong <tianshicong@zbintel.net>
2025-07-09 15:05:56 +0800

    登录url修改

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   baseUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:9527/' : 'https://auth.tiansc.top', // 后端接口地址
   cssUrl: process.env.NODE_ENV === 'development' ? '/api/' : '/api/',  // 生成的css文件地址
-  authUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : 'http://8.141.90.170/auth'
+  authUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : 'https://gate.tiansc.top/'
 }
